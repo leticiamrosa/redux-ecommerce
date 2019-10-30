e2712947b7570d98b8a6ee4201116d246d6f647f
leticia monteiro <leticia@vizir.com.br>
2019-10-30 16:30:37 -0300

    style: implementation routes and global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+import Routes from './routes';
 
 function App() {
     return (
-        <div>
-            <p>oi</p>
-        </div>
+        <BrowserRouter>
+            <GlobalStyle />
+            <Routes />
+        </BrowserRouter>
     );
 }
 
