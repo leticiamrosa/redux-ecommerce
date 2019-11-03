3ea40ae329e526594dfee56088ecdb4a7cd1bb2e
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2019-11-03 09:03:03 -0300

    feature: implementation redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import Header from './components/Header';
-
 import GlobalStyle from './styles/global';
-
 import Routes from './routes';
+import store from './store';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
-            <GlobalStyle />
-            <Routes />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Header />
+                <GlobalStyle />
+                <Routes />
+            </BrowserRouter>
+        </Provider>
     );
 }
 
