1d4918941d8bb77bb30b06ba3622165fc528e676
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2019-11-03 09:37:36 -0300

    feature: implementation debugger for redux (reactontron)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/ReactotronConfig';
+
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+
 import store from './store';
 
 function App() {
